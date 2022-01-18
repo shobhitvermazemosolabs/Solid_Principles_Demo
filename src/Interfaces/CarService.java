9963cf97533dca78b6964fb8f3f67bcb45be5ef2
Shobhit Verma <shobhit.verma@zemosolabs.com>
2022-01-18 15:28:14 +0530

    Added the Interfaces Seperately for the Demo of Interface Segregation and also defined a seperate class for the Demo of Single Responsibility

---
+++ src/Interfaces/CarService.java
@@ -0,0 +1,5 @@
+package Interfaces;
+
+public interface CarService {
+    public boolean serviceNeeded();
+}
---
+++ src/srp/Vehicle.java
@@ -0,0 +1,7 @@
+package srp;
+
+public class Vehicle {
+    public static void printVehicleType(String vehicleType){
+        System.out.println("The Vehicle Type is "+vehicleType);
+    }
+}
